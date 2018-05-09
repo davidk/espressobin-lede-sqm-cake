db3345efa7b6493fbb131628a5e6260d945fd713
davidk <davidk@users.noreply.github.com>
2018-05-08 18:58:49 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y screen tmux docker.io
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y screen docker.io
 sudo docker build -t espressobin .
 [ -d "/opt/openwrt" ] || sudo docker run -v /opt/openwrt:/opt:rw --entrypoint /install_sources.sh -it espressobin 
 exec screen sudo docker run -v /opt/openwrt:/opt:rw -u openwrt -w /opt/openwrt/openwrt-dd -it espressobin
